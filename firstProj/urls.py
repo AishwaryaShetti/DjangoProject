e162506333bc1b7779585cb30701dc586590f800
Aishwarya <aishwaryashetti01@gmail.com>
2023-04-11 14:13:38 +0530

    added template for word counter

--- firstProj/urls.py
+++ firstProj/urls.py
@@ -20,7 +20,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.home_page,name="home")
+    path("",views.home_page,name="home"),
+    path("count", views.counter, name="count"),
    
 ]
 
--- firstProj/views.py
+++ firstProj/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request,"home.html")
+def counter(request):
+    words = request.GET["sentence"]
+    number = len(words.split())
+    context = {"words": number}
+    return render(request, "count.html", context)
